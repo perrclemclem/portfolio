d582f395caf4a538baf7775346330d61109c6f15
Clem <clemenceperraudi@gmail.com>
2025-11-27 18:50:35 +0100

    final fix for psycopg2

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'mywebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD'],
