21473241ddbfc6ab06067408cdb4305fa35f2f10
Clem <clemenceperraudi@gmail.com>
2025-12-07 13:33:26 +0100

    changing settings to load images of the projects and css to make cv text visible

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -142,12 +142,13 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
